b1618d3e8f02aa140097a27de519ed6c51df50bd
muhammadkorra <muhammadkorra@gmail.com>
2017-12-12 02:56:28 +0200

    added the extract function

--- heap.c
+++ heap.c
@@ -59,6 +59,73 @@ int heap_insert (Heap *heap, const void *data){
 	}
 
 	heap->size ++;
-	
+
 	return 0;
+}
+
+int heap_extract (Heap *heap, void **data){
+	void *save, *temp;
+	int ipos, lpos, rpos, mpos;
+
+	if (heap_size(heap) == 0)
+		return -1;
+
+	*data = heap->tree[0];
+
+	save = heap->tree[heap_size(heap) - 1];
+
+	if (heap_size(heap) - 1 > 0){
+
+		if (temp = (void **) realloc (heap->tree, (heap_size(heap) - 1) * sizeof(void *)) == NULL) {
+			return -1;
+		}
+		else {
+			heap->tree = temp;
+		}
+
+		heap->size --;
+	}
+
+	else {
+		free(heap->tree);
+		heap->tree = NULL;
+		heap->size = 0;
+		return 0;
+	}
+
+	heap->tree[0] = save;
+
+	ipos = 0;
+	lpos = heap_left(ipos);
+	rpos = heap_right(ipos);
+
+	while (1){
+		lpos = heap_left(ipos);
+		rpos = heap_right(ipos);
+
+		if (lpos < heap_size(heap) && heap->compare(heap->tree[lpos],  heap->tree[ipos]) > 0){
+			mpos = lpos;
+		}
+		else{
+			mpos = ipos;
+		}
+
+		if (rpos < heap_size(heap) && heap->compare(heap->tree[rpos], heap->tree[mpos]) > 0){
+			mpos = rpos;
+		}
+
+		if (mpos == ipos){
+			break;
+		}
+		else{
+			temp = heap->tree[mpos];
+			heap->tree[mpos] = heap->tree[ipos];
+			heap->tree[ipos] = temp;
+
+			ipos = mpos;
+		}
+	}
+
+	return 0;
+
 }
