84444c2382b5cf206b4fadb6569e4d8b60b8179c
muhammadkorra <muhammadkorra@gmail.com>
2017-12-12 02:38:51 +0200

    added the insert function

--- heap.c
+++ heap.c
@@ -16,3 +16,49 @@ void heap_init (Heap *heap, int (*compare)(void *key1, void *key2),
 
 	return;
 }
+
+void heap_destroy(Head *heap){
+	int i;
+	if (heap->destroy != NULL){
+		for (i = 0; i < heap_size(heap); i++){
+			heap->destroy(heap->tree[i]);
+		}
+	}
+
+	free(heap->tree);
+	memset(heap, 0, sizeof(Heap));
+
+	return;
+}
+
+int heap_insert (Heap *heap, const void *data){
+	void *temp;
+
+	int ipos, ppos;
+
+	if((temp = (void**) realloc(heap->tree, (heap_size(heap) + 1) * sizeof (void*))) == NULL){
+		return -1;
+	}
+	else {
+		heap->tree = temp;
+	}
+
+	heap->tree[heap_size(heap)] = (void *) data;
+
+	ipos = heap_size(heap);
+	ppos = heap_parent(ipos);
+
+	while (ipos > 0 && heap->compare(heap->tree[ppos], heap->tree[ipos]) < 0){
+
+		temp = heap->tree[ppos];
+		heap->tree[ppos] = heap->tree[ipos];
+		heap->tree[ipos] = temp;
+
+		ipos = ppos;
+		ppos = heap_parent(ipos);
+	}
+
+	heap->size ++;
+	
+	return 0;
+}
